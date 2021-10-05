602a9524dc9d39f324d506fc9d5244ad867649b6
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2021-10-05 13:19:28 -0400

    clang-format

--- thread_pool.h
+++ thread_pool.h
@@ -1,37 +1,38 @@
 #ifndef THREAD_POOL_H
 #define THREAD_POOL_H
 
-#include <thread>
-#include <mutex>
-#include <thread>
-#include <condition_variable>
-#include <vector>
-#include <queue>
-#include <functional>
-#include <memory>
 #include <concepts>
+#include <condition_variable>
+#include <functional>
 #include <future>
+#include <memory>
+#include <mutex>
+#include <queue>
+#include <thread>
+#include <vector>
 
 class ThreadPool {
   public:
-    ThreadPool(size_t num_threads = std::thread::hardware_concurrency()): pool{}, task_queue{}, queue_mutex{}, manager{}, exit{false} {
+    ThreadPool(size_t num_threads = std::thread::hardware_concurrency())
+        : pool{}, task_queue{}, queue_mutex{}, manager{}, exit{false} {
 
-        auto worker =   [this] {
-                            while(true) {
-                                std::function<void(void)> task;
-                                {
-                                    std::unique_lock<std::mutex> lock(this->queue_mutex);
-                                    this->manager.wait(lock, [this]{ return this->exit || !this->task_queue.empty(); });
-                                    if(this->exit && this->task_queue.empty())
-                                    return;
-                                    task = std::move(this->task_queue.front());
-                                    this->task_queue.pop();
-                                }
-                                task();
-                            }
-                        };
+        auto worker = [this] {
+            while (true) {
+                std::function<void(void)> task;
+                {
+                    std::unique_lock<std::mutex> lock(this->queue_mutex);
+                    this->manager.wait(lock,
+                                       [this] { return this->exit || !this->task_queue.empty(); });
+                    if (this->exit && this->task_queue.empty())
+                        return;
+                    task = std::move(this->task_queue.front());
+                    this->task_queue.pop();
+                }
+                task();
+            }
+        };
 
-        for(size_t i = 0; i < num_threads; ++i)
+        for (size_t i = 0; i < num_threads; ++i)
             pool.emplace_back(worker);
     }
 
@@ -41,27 +42,28 @@ class ThreadPool {
             exit = true;
         }
         manager.notify_all();
-        for(auto& thread: pool)
+        for (auto& thread : pool)
             thread.join();
     }
 
-    template <std::invocable T, typename... Args>
-    void push_work(T&& t, Args&&... args) {
+    template <std::invocable T, typename... Args> void push_work(T&& t, Args&&... args) {
         {
             std::unique_lock<std::mutex> lock(queue_mutex);
-            task_queue.push([t](){t(args...);});
+            task_queue.push([t]() { t(args...); });
         }
         manager.notify_one();
     }
 
     template <typename F, typename... Args>
-    [[nodiscard]] auto push_task(F&& f, Args&&... args)-> std::future<std::invoke_result_t<F, Args...>> {
+    [[nodiscard]] auto push_task(F&& f, Args&&... args)
+        -> std::future<std::invoke_result_t<F, Args...>> {
         using return_type = std::invoke_result_t<F, Args...>;
-        auto task = std::make_shared<std::packaged_task<return_type()>>(std::bind(std::forward<F>(f), std::forward<Args>(args)...));
+        auto task = std::make_shared<std::packaged_task<return_type()>>(
+            std::bind(std::forward<F>(f), std::forward<Args>(args)...));
         std::future<return_type> result = task->get_future();
         {
             std::unique_lock<std::mutex> lock(queue_mutex);
-            task_queue.push([task](){(*task)();});
+            task_queue.push([task]() { (*task)(); });
         }
         manager.notify_one();
         return result;
@@ -76,5 +78,4 @@ class ThreadPool {
     bool exit;
 };
 
-
-#endif //THREAD_POOL_H
+#endif // THREAD_POOL_H
