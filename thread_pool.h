763cc2e83f5d55f13e9d79f0c4d6435c42ef5e71
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2021-10-05 13:13:22 -0400

    works with args and return types now

--- thread_pool.h
+++ thread_pool.h
@@ -10,18 +10,19 @@
 #include <functional>
 #include <memory>
 #include <concepts>
+#include <future>
 
 class ThreadPool {
   public:
-    ThreadPool(size_t num_threads = std::thread::hardware_concurrency()): pool{}, task_queue{}, queue_mutex{}, manager{}, stop{false} {
+    ThreadPool(size_t num_threads = std::thread::hardware_concurrency()): pool{}, task_queue{}, queue_mutex{}, manager{}, exit{false} {
 
         auto worker =   [this] {
                             for(;;) {
                                 std::function<void()> task;
                                 {
                                     std::unique_lock<std::mutex> lock(this->queue_mutex);
-                                    this->manager.wait(lock, [this]{ return this->stop || !this->task_queue.empty(); });
-                                    if(this->stop && this->task_queue.empty())
+                                    this->manager.wait(lock, [this]{ return this->exit || !this->task_queue.empty(); });
+                                    if(this->exit && this->task_queue.empty())
                                     return;
                                     task = std::move(this->task_queue.front());
                                     this->task_queue.pop();
@@ -37,20 +38,33 @@ class ThreadPool {
     ~ThreadPool() {
         {
             std::unique_lock<std::mutex> lock(queue_mutex);
-            stop = true;
+            exit = true;
         }
         manager.notify_all();
         for(auto& thread: pool)
             thread.join();
     }
 
-    template <std::invocable T>
-    void push(T&& task) {
+    template <std::invocable T, typename... Args>
+    void push_work(T&& t, Args&&... args) {
         {
             std::unique_lock<std::mutex> lock(queue_mutex);
-            task_queue.emplace(task);
+            task_queue.push([t](){t(args...);});
         }
         manager.notify_one();
+    }
+
+    template <typename F, typename... Args>
+    auto push_task(F&& f, Args&&... args)-> std::future<std::invoke_result_t<F, Args...>> {
+        using return_type = std::invoke_result_t<F, Args...>;
+        auto task = std::make_shared<std::packaged_task<return_type()>>(std::bind(std::forward<F>(f), std::forward<Args>(args)...));
+        std::future<return_type> result = task->get_future();
+        {
+            std::unique_lock<std::mutex> lock(queue_mutex);
+            task_queue.push([task](){(*task)();});
+        }
+        manager.notify_one();
+        return result;
     }
 
   private:
@@ -59,7 +73,7 @@ class ThreadPool {
 
     std::mutex queue_mutex;
     std::condition_variable manager;
-    bool stop;
+    bool exit;
 };
 
 
